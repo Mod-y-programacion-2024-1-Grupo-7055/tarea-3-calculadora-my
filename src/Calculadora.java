65ecd9e4d81bdd1b70734cfa0263ecd39d64e4eb
Yelab Darío Gaytán Pazos <yelab@ciencias.unam.mx>
2023-11-20 00:10:44 -0600

    Calculadora sin que broten nuevas instancias

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -533,12 +533,7 @@ public class Calculadora extends javax.swing.JFrame {
             }
             //</editor-fold>
     
-            /* Create and display the form */
-            java.awt.EventQueue.invokeLater(new Runnable() {
-                public void run() {
-                    new Calculadora().setVisible(true);
-                }
-            });
+            
         }
     
         // Variables declaration - do not modify                     
